05cb24c8b70c727c1b620eb9957388ee71b8a7f6
Maksim1108 <maksim.lyanko@bk.ru>
2020-11-09 14:35:13 +0600

    add section "blog"

--- js/index.js
+++ js/index.js
@@ -1,31 +1,31 @@
-// let item1 = document.getElementById('item1');
-// let item2 = document.getElementById('item2');
-// let item3 = document.getElementById('item3');
-// let item4 = document.getElementById('item4');
+let item1 = document.getElementById('item1');
+let item2 = document.getElementById('item2');
+let item3 = document.getElementById('item3');
+let item4 = document.getElementById('item4');
+let icon1 = document.getElementById('icon1');
 
-// let icon1 = document.getElementById('icon1');
-// let icon2 = document.getElementById('icon2');
-// let icon3 = document.getElementById('icon3');
-// let icon4 = document.getElementById('icon4');
+let icon2 = document.getElementById('icon2');
+let icon3 = document.getElementById('icon3');
+let icon4 = document.getElementById('icon4');
 
-// let link1 = document.getElementById('link1');
-// let link2 = document.getElementById('link2');
-// let link3 = document.getElementById('link3');
-// let link4 = document.getElementById('link4');
+let link1 = document.getElementById('link1');
+let link2 = document.getElementById('link2');
+let link3 = document.getElementById('link3');
+let link4 = document.getElementById('link4');
 
-// let items = [item1, item2, item3, item4];
-// let icons = [icon1, icon2, icon3, icon4];
-// let links = [link1, link2, link3, link4];
+let items = [item1, item2, item3, item4];
+let icons = [icon1, icon2, icon3, icon4];
+let links = [link1, link2, link3, link4];
 
-// items.forEach((items) => items.addEventListener('mouseenter', () => {
-//     icons.classList.add('greenGradient');
-//     links.classList.add('greenGradient');
-// }));
+items.forEach((items) => items.addEventListener('mouseenter', () => {
+    icons.classList.add('greenGradient');
+    links.classList.add('greenGradient');
+}));
 
-// items.forEach((items) => items.addEventListener('mouseleave', () => {
-//     icons.classList.remove('greenGradient');
-//     links.classList.remove('greenGradient');
-// }));
+items.forEach((items) => items.addEventListener('mouseleave', () => {
+    icons.classList.remove('greenGradient');
+    links.classList.remove('greenGradient');
+}));
 
 const accordion = document.getElementsByClassName('faq__contentBox')
 
